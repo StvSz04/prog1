87f85303c0baf6e4250da54d4d9020eda1e60210
Steven <iamstevensanchez04@gmail.com>
2025-01-17 21:41:21 -0600

    Added code to read in data

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
-#include <pthread.h>
+
+
+//Populate the 2d array to then print out in main
+void decode(int width, int height, int* headPos, int* dataPos, int* char_vector, int* range_vector){
+    
+}
 
 int main(){
-    std::cout << "Hello World";
-    return 0;
+    // Read in size parameters
+    std::string size_parameters;
+    std::getline(std::cin, size_parameters);
+    
+    std::cout << size_parameters;
+    
+    // Read in char and their ranges
+    std::string char_parameters;
+    std::getline(std::cin, char_parameters);
+    
+    std::cout << char_parameters;
+
+    // Read in headPos data
+    std::string headPos;
+    std::getline(std::cin, headPos);
+    
+    std::cout << headPos;
+
+
+    // Read in dataPos data
+    std::string dataPos;
+    std::getline(std::cin, dataPos);
+    
+    std::cout << dataPos;
+
+
+
+
+
+ 
+    
 }
