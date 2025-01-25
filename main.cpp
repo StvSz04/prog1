7ec7ba5e7f5dc37f456af3656f7206d8de72732e
Steven <iamstevensanchez04@gmail.com>
2025-01-24 22:57:19 -0600

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 
+// Define a struct that holds corresponding symbols and ranges
 struct symbolData
 {
-    // Fix these data types later
    std::string symbol;
    std::vector<int> range;
 };
 
-// A vector of type symbolData(Struct)
+// A vector of type symbolData to store instances of symbolData
 std::vector<symbolData> symbolDataVec;
 
 //Populate the 2d array to then print out in main
@@ -17,73 +17,79 @@ void decode(int width, int height, int* headPos, int* dataPos, int* char_vector,
 }
 
 int main(){
-    // Read in size parameters
-    int length;
-    int width;
+   
+    int length;// Define an int to hold length
+    int width; // Define an int to hold width
+    std::cin >> length; // Read in Length
+    std::cin >> width; // Read in width
     
-    std::cin >> length;
-    std::cin >> width;
+    std::string symbolString; // Define a string to hold the input data corresponding to all symbols and ranges
+    std::cin.ignore();       // Clear the input stream of \n
+    std::getline(std::cin, symbolString); // Read in the entire lin containing data corresponding to all symbols and ranges
+    int symbolStringLen = symbolString.length(); // Retrieve the length of symbolString to use for a for loop
     
+    char x; // Define a char to hold each char within symbolString when extracting inside the for loop
     
-    
-    std::string symbolString;
-    std::cin.ignore();
-    std::getline(std::cin, symbolString);
-    int symbolStringLen = symbolString.length();
-    
-    
-    // Test symbol data input collection
-    char x;
+    // A for loop to extract all characters and intergers from symbolString
     for(int i=0; i < symbolStringLen; i++){
        
-       x = symbolString[i];
+       x = symbolString[i]; // Assign x to the current index in symbolString
        
+       // If x is a space char then return to begining of loop
        if(x == ' '){
           // Return to for loop
        }
        
+       // At the begining of the loop execute these lines
        else if(i == 0){
-           symbolData instance;
-           instance.symbol = x;
-           symbolDataVec.push_back(instance);
+           symbolData instance; // Create a new instance of symbolData
+           instance.symbol = x; // Assign the symbol memeber of the instance to x
+           symbolDataVec.push_back(instance); // push the new instance of symbolData to the vector symbolDataVec
        }        
        
+       // If the current char(x) is a comma then execute these lines
        else if(x == ','){
-           symbolData instance;
-           x = symbolString[i + 1];
-           instance.symbol = x;
-           symbolDataVec.push_back(instance);
-           i++;
+           symbolData instance; // Create a new instance of symbolData
+           x = symbolString[i + 1]; // Assign x to the next character in the string(symbolString) which will be a letter/symbol
+           instance.symbol = x;  // Assign the symbol memeber of the instance to x
+           symbolDataVec.push_back(instance); // push the new instance of symbolData to the vector symbolDataVec
+           i++; // increment index(i) to account for reading in the next character within symbolString
 
            
         
        }
+       // This branch handles reading and storing ints 
        else{
-           std::string stringToInt = "";
+           std::string stringToInt = ""; // Define and instantiate a string to temporarily hold the ints read in from symbolString
+          // Loop to extract all parts of an int for example if the int is 10 it will read in 1 and then 0 using the loop
            while(true){
+               // Break the loop if a space is encountered
                if( x == ' '){
                     break;
                 }
+                // Break the loop if a comma is encountered and decrement index(i) to account for previous in while loop incrementation
                 else if(x == ','){
                     i--;
                     break;
                 }
-                stringToInt += x;
-                x = symbolString[i + 1];
-                i++;
+                stringToInt += x; // Append the char that contains a int to stringToInt
+                x = symbolString[i + 1]; // Assign x to the next char in stringToInt
+                i++; // Increment i to account due to the previous step to maintain the for loop logic
            }
            
+           // Once all parts of the int have been extracted then convert the string(stringToInt) to a int 
+           // and assign the last instance of symbolData's range to this value
            if(stringToInt != ""){
-               int  y = std::stoi(stringToInt);
-               symbolDataVec.back().range.push_back(y);
-           }
+               int  temp = std::stoi(stringToInt); // Convert stringToInt, using stoi, to and int. Store in a temp var (temp)
+               symbolDataVec.back().range.push_back(temp); // Assign the int value(temp) to the last instance of a symbolData struct's range member in symbolDataVec 
+           }                                                // Last value because a "read" number will always correspond to the last instance of a symbolData struct instance
        }
        
        
     }
 
     // Read in headPos data
-    std::string headPos;
+    std::string headPos; // Define a string to hold HeadPos data
     std::cin.ignore();
     std::getline(std::cin, headPos);
 
