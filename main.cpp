268981bb5c10b4071f7e3b1170df63b8093fa467
Steven <iamstevensanchez04@gmail.com>
2025-01-29 17:01:11 -0600

    This version satisfies the output for any input

--- main.cpp
+++ main.cpp
@@ -1,32 +1,61 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 // Define a struct that holds corresponding symbols and ranges
-struct symbolData
-{
-   std::string symbol;
+struct symbolData{
+   char symbol;
    std::vector<int> range;
 };
 
+struct inputData{
+    int length;
+    int width; 
+    std::vector<symbolData> symbolDataVector; 
+    std::vector<int> headPosVector;
+    std::vector<int> dataPosVector;
+};
 
-//Populate the 2d array to then print out in main
-void decode(int width, int length, const std::vector<symbolData>& symbolDataVec, const std::vector<int>& headPosVector, const  std::vector<int>& dataPosVector ){
-    
+
+//Decode the input data and return the 2d vector containing the ascii values
+std::vector<std::vector<char>> decode(int length, int width, const std::vector<symbolData>& symbolDataVec, const std::vector<int>& headPosVector, const  std::vector<int>& dataPosVector){
+    std::vector<std::vector<char>> outputVector(length, std::vector<char>(width, ' ')); // Create a 2d vector to hold positions of decoded ascii chars
+    //Loop each row
+    for(int i = 0; i < length; i++){
+        int range;
+        
+        if(i == length - 1){
+            range = dataPosVector.size();
+        }
+        else{
+            range = headPosVector[i + 1];
+        }
+        // Loop each necessary index in dataPosVector
+        for(int j= headPosVector[i]; j < range; j++){
+            //Loop for each symbol
+            for(int y=0; y < symbolDataVec.size(); y++){
+                //check all ranges for each symbol
+                for(int z=0; z <=symbolDataVec[y].range.size()/2; z++){
+                    if(dataPosVector[j] >= symbolDataVec[y].range[z] && dataPosVector[j] <= symbolDataVec[y].range[z + 1]){
+                        outputVector[i][dataPosVector[j]] = symbolDataVec[y].symbol;
+                        z++;
+                    }
+                }
+            }
+            
+        }
+       
+    }
+    return outputVector;
+}   
+
+int getSizeParameter(){
+    int sizeParameter;
+    std::cin >> sizeParameter;
+    return sizeParameter;
 }
 
-int main(){
-    // A vector of type symbolData to store instances of symbolData
-    std::vector<symbolData> symbolDataVector;
-    // A vector to store headPos data
-    std::vector<int> headPosVector;
-    // A vector to store dataPos data
-    std::vector<int> dataPosVector;
-   
-    int length;// Define an int to hold length
-    int width; // Define an int to hold width
-    std::cin >> length; // Read in Length
-    std::cin >> width; // Read in width
-    
+void  getSymbolData(std::vector<symbolData>& symbolDataVector){
     std::string symbolString; // Define a string to hold the input data corresponding to all symbols and ranges
     std::cin.ignore();       // Clear the input stream of \n
     std::getline(std::cin, symbolString); // Read in the entire lin containing data corresponding to all symbols and ranges
@@ -91,60 +120,75 @@ int main(){
        
        
     }
+    
+}
 
-    // Read in headPos data
-    std::string headPosData; // Define a string to hold HeadPos data
-    std::getline(std::cin, headPosData);
-    int headPosDataLen = headPosData.length();
+//Retreive headPos or DataPos data
+void getPosData(std::vector<int>& tempPosVector){
+    std::string posData; // Define a string to hold HeadPos data
+    char x;
+    std::getline(std::cin, posData);
+    int posDataLen = posData.length();
     std::string temp; //String to temp hold numbers
     
-    for(int i = 0; i < headPosDataLen; i++){
+    for(int i = 0; i < posDataLen; i++){
         temp = "";
-        x = headPosData[i];
+        x = posData[i];
         while(true){
             if( x == ' '){
                 break;
                 }
-            else if(i == headPosDataLen){
+            else if(i == posDataLen){
                  break;
              }
         
             temp += x;
-            x = headPosData[i + 1];
+            x = posData[i + 1];
             i++;
                 
         }
         if(temp != ""){
             int num = std::stoi(temp);
-            headPosVector.push_back(num);
+            tempPosVector.push_back(num);
         }
     }
-
-    // Read in dataPos data
-    std::string dataPosData;
-    std::getline(std::cin, dataPosData);
-    int dataPosDataLen = dataPosData.length();
     
-    for(int i = 0; i < dataPosDataLen; i++){
-        temp = "";
-        x = dataPosData[i];
-        
-        while(true){
-            if( x == ' '){
-                 break;
-                }
-            else if(i == dataPosDataLen){
-                 break;
-             }
-            temp += x;
-            x = headPosData[i + 1];
-            i++;
-        }
-        if(temp != ""){
-            int num = std::stoi(temp);
-            dataPosVector.push_back(num);
-        }
-    }
+}
+
+int main(){
+    // A vector of type symbolData to store instances of symbolData
+    std::vector<symbolData> symbolDataVector;
+    // A vector to store headPos data
+    std::vector<int> headPosVector;
+    // A vector to store dataPos data
+    std::vector<int> dataPosVector;
+    
+    int length;// Define an int to hold length (# of rows)
+    int width; // Define an int to hold width ( # of cols)
+    
+    // Get all data from input
+    width = getSizeParameter();
+    length = getSizeParameter();
+    getSymbolData(symbolDataVector);
+    getPosData(headPosVector);
+    getPosData(dataPosVector);
+    
+    // Initialize struct of type inputData
+    inputData inputDataInstance;
+    inputDataInstance.width = width;
+    inputDataInstance.length = length;
+    inputDataInstance.symbolDataVector = symbolDataVector;
+    inputDataInstance.headPosVector = headPosVector;
+    inputDataInstance.dataPosVector = dataPosVector;
+    
     // Call the decode function
-    decode(width, length, symbolDataVector, headPosVector, dataPosVector);
+    std::vector<std::vector<char>> outputVector =decode(length, width, symbolDataVector, headPosVector, dataPosVector);
+    //Print 2d vector
+    for (int i = 0; i < length; i++) {  // Iterate over rows
+        for (int j = 0; j < width; j++) {  // Iterate over columns
+            std::cout << outputVector[i][j];  // Print the value at (i, j)
+        }
+        std::cout << std::endl;  // Print a newline after each row
+    }
+    
 }
